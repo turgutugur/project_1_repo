424bbcc236f2aa5c181f5494082df8a5d57a0415
Turgut Ozsirkinti <turgutugur@msn.com>
2022-02-03 21:23:42 -0500

    adding new file

---
+++ api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "68a18eb052522492e2324d052196c61c"
+
+# Google API Key
+g_key = "AIzaSyClmTFYYbIHihNk7wJihE9cNTKU_XSm4FY"
+
+# Census API Key
+c_key = "9f68a41ea2d7e4e37a8b658738d97abab244023b"
